f6a7ffc71e5ce797190572cc92953e0ed0836269
unknown <gota.adssa@gmail.com>
2026-09-01 08:02:49 -0300

    started to add the User class

---
+++ test.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, name, age, color, gender, hobby, cereal):
+        self.name = name
+        self.age = age
+        self.color = color
+        self.gender = gender
+        self.hobby = hobby
+        self.cereal = cereal
+
+    def your_avg(self):
+        
