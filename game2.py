71a59a1ed2d289617c5354ee89e157b793fd1be9
unknown <gota.adssa@gmail.com>
2026-09-11 08:10:17 -0300

    Rock Paper Scissors is functional

--- game2.py
+++ game2.py
@@ -1,22 +1,33 @@
 ## Game 2: Rock Paper Scissors
 
 import random
-possible = ['1','2','3']
+import time
+possible = ['0','1','2']
+play_meaning = {'0': 'Rock', '1': 'Paper', '2': 'Scissors'}
 
 while True:
-    play = input("What will you play?\n1. Rock ✊\n2. Paper 🖐️\n3. Scissors ✌️\n4. Quit\n")
-    if play == '4':
+    play = input("What will you play?\n0. Rock ✊\n1. Paper 🖐️\n2. Scissors ✌️\n3. Quit\n")
+    if play in ['3', 'Quit', 'quit']:
         quit()
     elif not play in possible:
-        print("Pick 1, 2 or 3 please")
+        print("Pick 0, 1 or 2 please")
     else:
         break
 
+time.sleep(1)
 pc_play = random.choice(possible)
-print(pc_play)
-res = (int(play) - int(pc_play)) % 3
-print(res)
+print('You picked {} and PC picked {}'.format(play_meaning[play], play_meaning[pc_play]))
 
-""" if play == pc_play:
-    print("It's a tie!")
-elif: """
+def whoWon(refplayer, otherplayer):
+    ans = (int(refplayer) - int(otherplayer)) % 3
+    if ans == 0:
+        print("It's a Tie!")
+    elif ans == 1:
+        print("You Win!")
+    else:
+        print("You Lose!")
+        
+time.sleep(1)
+the_game = whoWon(play, pc_play)
+
+
